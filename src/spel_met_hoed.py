c8d9016dd35bdd8390c0abf11d017996b3a23ec1
Fieke <fiekeh@github.com>
2020-04-22 22:54:29 +0200

    Bug fixes in puntentelling & ronde count

--- src/spel_met_hoed.py
+++ src/spel_met_hoed.py
@@ -35,22 +35,23 @@ class SpelMetDeHoed:
         """
         if self.last_woord is not None and len(self.woorden_in_ronde) > 0:
             self.woorden_in_ronde.remove(self.last_woord)
+            self.team_scores[self.team_index_aan_beurt] += 1  # Houdt de score bij
 
         if len(self.woorden_in_ronde) > 0:
             nieuw_woord = random.choice(self.woorden_in_ronde)
             self.last_woord = nieuw_woord  # Onthoudt laatste woord om straks te kunnen verwijderen
-            self.team_scores[self.team_index_aan_beurt] += 1  # Houdt de score bij
         else:
             return None
 
         return nieuw_woord
 
-    def start_nieuwe_ronde(self):
+    def start_nieuwe_ronde(self, count_up=True):
         """Functie om een nieuwe ronde te starten.
         --> Reset de woorden in ronde naar alle woorden in hoed."""
         self.woorden_in_ronde = self.woorden_in_hoed.copy()
         self.last_woord = None
-        self.ronde_count += 1
+        if count_up:
+            self.ronde_count += 1
 
     def start_nieuwe_beurt(self):
         """Functie om een nieuwe beurt te krijgen.
@@ -59,6 +60,7 @@ class SpelMetDeHoed:
         self.team_index_aan_beurt += 1
         if self.team_index_aan_beurt >= len(self.teams):
             self.team_index_aan_beurt = 0
+        self.last_woord = None
 
 
 @app.route("/voeg_woorden_toe", methods=['GET', 'POST'])
@@ -70,7 +72,7 @@ def verzamel_de_input():
         if woord is not '':
             flash('Yeah, je woord zin in de hoed! ' + woord)
             spel.woorden_in_hoed.append(woord)
-            spel.start_nieuwe_ronde()
+            spel.start_nieuwe_ronde(count_up=False)
         else:
             flash('Enter een valide woord! Dit is geen woord..')
     return render_template('verzamel_woorden.html', form=form)
@@ -83,8 +85,8 @@ def speel_het_spel():
     if request.method == 'POST':
         if request.form['submit_button'] == 'Nieuwe ronde':
             spel.start_nieuwe_ronde()
-            flash('Tijd voor een nieuwe ronde!! '
-                  'Team {} is aan de beurt!'.format(spel.teams[spel.team_index_aan_beurt]))
+            flash('Tijd voor ronde nummer {}!! '
+                  'Team {} is aan de beurt!'.format(spel.ronde_count, spel.teams[spel.team_index_aan_beurt]))
 
         if request.form['submit_button'] == 'Nieuwe beurt':
             spel.start_nieuwe_beurt()
